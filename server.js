b2d4c7cce2da9ffe4a51ece4faca29a65ae17fca
halaburdataras <Taras.Galaburda@outlook.com>
2021-12-01 19:58:03 +0200

    ci: Empty commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { applyMiddleware } = require('graphql-middleware');
 const cors = require('cors');
 const config = require('config');
+
 const connectDB = require('./config/db');
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
